5c8c9032b2b78be79f55f09a5981d6e5e0259c50
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-07-23 20:01:44 +0200

    add tests for Blog model; check tags property

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -68,3 +68,73 @@ def test_blog_articles_only_live():
 
     assert post_1 in blog.articles
     assert post_2 not in blog.articles
+
+@pytest.mark.django_db
+def test_blog_exposes_empty_tags_as_default():
+    """
+    The Blog model can retrieve a list of all available tags.
+    By default, a Post doesn't have a Tag.
+        - the Blog page is a child of the root page
+        - the Blog page contains a post
+        - blog.tags must return an empty list
+    """
+    # create a blog post with two posts
+    root_page = get_root_page()
+    blog = mommy.prepare_recipe('tests.recipes.blog')
+    post = mommy.prepare_recipe('tests.recipes.post')
+    link_page(root_page, blog)
+    link_page(blog, post)
+
+    assert len(blog.tags) == 0
+
+@pytest.mark.django_db
+def test_blog_exposes_articles_tags():
+    """
+    The Blog model can retrieve a list of all available tags.
+        - the Blog page is a child of the root page
+        - the Blog page contains a post
+        - the Post has two Tag
+        - blog.tags must return two tags
+    """
+    # create a blog page with a post
+    root_page = get_root_page()
+    blog = mommy.prepare_recipe('tests.recipes.blog')
+    post = mommy.prepare_recipe('tests.recipes.post')
+    link_page(root_page, blog)
+    link_page(blog, post)
+    # append two tags to the post
+    tag_1 = mommy.make('blog.PostTag', content_object=post).tag
+    tag_2 = mommy.make('blog.PostTag', content_object=post).tag
+
+    assert len(blog.tags) == 2
+    assert tag_1 in blog.tags
+    assert tag_2 in blog.tags
+
+@pytest.mark.django_db
+def test_blog_exposes_articles_tags_are_order():
+    """
+    The Blog model can retrieve a list of all available tags.
+    The list of tags is ordered according to the usage so that
+    more used tags are listed first.
+        - the Blog page is a child of the root page
+        - the Blog page contains two posts
+        - post_1 has two tags, post_2 has one tags
+        - a tag is used twice while the other only once
+        - blog.tags must return two tags; the first one should be
+          the most common
+    """
+    # create a blog page with two posts
+    root_page = get_root_page()
+    blog = mommy.prepare_recipe('tests.recipes.blog')
+    posts = mommy.prepare_recipe('tests.recipes.post', _quantity=2)
+    link_page(root_page, blog)
+    link_page(blog, posts)
+    # append two tags to two different posts
+    tag_1 = mommy.make('blog.PostTag', content_object=posts[0]).tag
+    tag_2 = mommy.make('blog.PostTag', content_object=posts[1]).tag
+    # one tag is used twice
+    mommy.make('blog.PostTag', tag=tag_2, content_object=posts[0])
+
+    assert len(blog.tags) == 2
+    assert blog.tags[0] == tag_2
+    assert blog.tags[1] == tag_1
