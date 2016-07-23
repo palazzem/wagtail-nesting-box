8b62b7032f934c7e2a726f3c1ae14592b9b32960
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-07-23 13:34:48 +0200

    bootstrap tests environment with py.test

---
+++ tests/conftest.py
@@ -0,0 +1,79 @@
+def pytest_configure():
+    from django.conf import settings
+
+    settings.configure(
+        DEBUG_PROPAGATE_EXCEPTIONS=True,
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:'
+            }
+        },
+        SITE_ID=1,
+        SECRET_KEY='not_very_secret_in_tests',
+        WAGTAIL_SITE_NAME = 'test-site',
+        USE_I18N=True,
+        USE_L10N=True,
+        STATIC_URL='/static/',
+        ROOT_URLCONF='tests.urls',
+        TEMPLATES=[
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'APP_DIRS': True,
+                'OPTIONS': {
+                    'context_processors': [
+                        'django.template.context_processors.debug',
+                        'django.template.context_processors.request',
+                        'django.contrib.auth.context_processors.auth',
+                        'django.contrib.messages.context_processors.messages',
+                    ],
+                },
+            },
+        ],
+        MIDDLEWARE_CLASSES=(
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.middleware.common.CommonMiddleware',
+            'django.middleware.csrf.CsrfViewMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+            'django.middleware.clickjacking.XFrameOptionsMiddleware',
+            'django.middleware.security.SecurityMiddleware',
+
+            'wagtail.wagtailcore.middleware.SiteMiddleware',
+            'wagtail.wagtailredirects.middleware.RedirectMiddleware',
+        ),
+        INSTALLED_APPS=(
+            'wagtail_box',
+            'wagtail_box.blog',
+
+            'wagtail.contrib.settings',
+            'wagtail.wagtailforms',
+            'wagtail.wagtailredirects',
+            'wagtail.wagtailembeds',
+            'wagtail.wagtailsites',
+            'wagtail.wagtailusers',
+            'wagtail.wagtailsnippets',
+            'wagtail.wagtaildocs',
+            'wagtail.wagtailimages',
+            'wagtail.wagtailsearch',
+            'wagtail.wagtailadmin',
+            'wagtail.wagtailcore',
+
+            'modelcluster',
+            'taggit',
+
+            'django.contrib.admin',
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.messages',
+            'django.contrib.staticfiles',
+        ),
+    )
+
+    try:
+        import django
+        django.setup()
+    except AttributeError:
+        pass
