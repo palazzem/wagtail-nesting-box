f85236265da78b61d26a4f4e8c52e992e1076f06
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-07-23 11:30:14 +0200

    bootstrap wagtail_box package

---
+++ wagtail_box/__init__.py
@@ -0,0 +1,12 @@
+"""
+Wagtail nesting box
+"""
+
+__title__ = 'Wagtail nesting box'
+__version__ = '0.0.0'
+__author__ = 'Emanuele Palazzetti'
+__license__ = 'BSD 2-Clause'
+__copyright__ = 'Copyright 2016 Emanuele Palazzetti'
+
+# Version synonym
+VERSION = __version__
