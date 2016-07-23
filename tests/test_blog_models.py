141406ee0daf3f3ff25c0bf42238448351570fcf
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-07-23 13:50:00 +0200

    add some tests for the Blog and Post models

---
+++ tests/test_blog_models.py
@@ -0,0 +1,30 @@
+from wagtail.wagtailcore.models import Page
+from wagtail.tests.utils import WagtailPageTests
+
+from wagtail_box.blog.models import Blog, Post
+
+
+class BlogPageTest(WagtailPageTests):
+    def test_blog_child_of_anything(self):
+        """
+        Ensures that the Blog page can be child of any kind of Page
+        """
+        self.assertCanCreateAt(Page, Blog)
+
+    def test_post_child_of_blog(self):
+        """
+        Ensures that a Post page can be a child of Blog
+        """
+        self.assertCanCreateAt(Blog, Post)
+
+    def test_post_cannot_be_child_of_anything(self):
+        """
+        Ensures that a Post page cannot be a child of a generic page
+        """
+        self.assertCanNotCreateAt(Page, Post)
+
+    def test_post_cannot_have_children(self):
+        """
+        Ensures that a Post page cannot have any kind of children
+        """
+        self.assertCanNotCreateAt(Post, Page)
